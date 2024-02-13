1a68b430553389a329d73a0626be150d511c2029
ranss@rambler.ru <ranss@rambler.ru>
2024-02-13 23:14:45 +0300

    init commit

---
+++ src/main/java/com/example/tgbotbusboro/TgbotBusBoroApplication.java
@@ -0,0 +1,13 @@
+package com.example.tgbotbusboro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgbotBusBoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgbotBusBoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tgbotbusboro/TgbotBusBoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgbotbusboro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotBusBoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
